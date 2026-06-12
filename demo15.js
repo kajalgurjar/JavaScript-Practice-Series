14ffe762dbc8ab5d6965cd332c4c959566d569ca
kajalgurjar <kajalgurjar16122002@gmail.com>
2026-06-12 22:35:14 +0530

    add  Declare variables using var, let, and const

---
+++ demo15.js
@@ -0,0 +1,20 @@
+// Declare variables using var, let, and const.
+
+// var declaration
+var name = "Kajal";
+console.log("Name:", name);
+
+// let declaration
+let age = 23;
+console.log("Age:", age);
+
+// const declaration
+const country = "India";
+console.log("Country:", country);
+
+// Updating var and let values
+name = "Priya";
+age = 26;
+
+console.log("Updated Name:", name);
+console.log("Updated Age:", age);
