5e88dd14e6efa20980c77256deb4359cc81bb3dc
kajalgurjar <kajalgurjar16122002@gmail.com>
2026-06-15 22:50:57 +0530

    add data type program

--- demo13.js
+++ demo13.js
@@ -10,4 +10,4 @@ function debounce(func, delay){
          func();
       }, delay);
    }
-}
+} 
---
+++ demo16.js
@@ -0,0 +1,18 @@
+// Find the data type of a variable.
+
+let x = 42;
+console.log(typeof x); // Output: "number"  
+let y = "Hello, World!";
+console.log(typeof y);
+// Output: "string"
+let z = true;
+console.log(typeof z);
+// Output: "boolean"
+let a = null;
+console.log(typeof a);
+// Output: "object" (this is a quirk in JavaScript)
+let b;
+console.log(typeof b);
+// Output: "undefined"
+let c = { name: "Alice", age: 30 };
+console.log(typeof c);
